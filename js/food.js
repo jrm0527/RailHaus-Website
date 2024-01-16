ff217a6d0c581660b8bc70b8b571a673e6a131af
jrm0527 <geeziegames@gmail.com>
2024-01-16 16:22:49 -0500

    reworked food.js to import json data from gotab api

--- js/food.js
+++ js/food.js
@@ -1,30 +1,40 @@
-function setupPage() {
-  fetch("../js/food.json")
+const menus = [
+  "burgers",
+  "desserts",
+  "kids",
+  "mains",
+  "salads",
+  "starters",
+  "wurst",
+];
+
+function setupPage(menu) {
+  fetch(`../json/${menu}.json`)
     .then((response) => response.json())
     .then((foods) => {
       for (let i = 0; i < foods.length; i++) {
         let food = foods[i];
-        createFoodMenu(food);
+        createFoodMenu(menu, food);
       }
     });
 }
 
-function createFoodMenu(food) {
+function createFoodMenu(menu, food) {
   let name = document.createElement("span");
   name.innerText = food.name;
   name.classList.add("item-name");
 
   let detail = document.createElement("span");
   detail.classList.add("item-detail");
-  detail.innerText = food.detail;
+  detail.innerText = food.description;
 
   let li = document.createElement("li");
   li.classList.add("list-item");
   li.appendChild(name);
   li.appendChild(detail);
 
-  const list = document.getElementById(`${food.type}-list`);
+  const list = document.getElementById(`${menu}-list`);
   list.appendChild(li);
 }
 
-setupPage();
+menus.forEach(setupPage);
