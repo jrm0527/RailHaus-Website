1008aa753f3218a72bc3ec6417b12163a27d2f35
jrm0527 <geeziegames@gmail.com>
2024-01-19 12:03:56 -0500

    reworked drink menu to use gotab API data

--- js/drinks.js
+++ js/drinks.js
@@ -1,50 +1,64 @@
-function setupPage() {
-  fetch("../js/drinks.json")
+const menus = [
+  "beer-cocktails",
+  "cans-bottles",
+  "cocktails",
+  "flights",
+  "main-bar-draft",
+  "spirits",
+  "wine",
+];
+
+function setupPage(menu) {
+  fetch(`../json/${menu}.json`)
     .then((response) => response.json())
     .then((drinks) => {
       for (let i = 0; i < drinks.length; i++) {
-        if (drinks[i].type != "yard-bar-draft") {
-          let drink = drinks[i];
-          createDrinkMenu(drink);
-        }
+        let drink = drinks[i];
+        createDrinkMenu(menu, drink);
       }
     });
 }
 
-function createDrinkMenu(drink) {
+function createDrinkMenu(menu, drink) {
   let name = document.createElement("span");
   name.innerText = drink.name;
   name.classList.add("item-name");
 
-  let size = document.createElement("span");
-  size.classList.add("item-size");
-  size.innerText = drink.size;
-
-  let nameCont = document.createElement("li");
-  nameCont.classList.add("list-name");
-  nameCont.appendChild(name);
-  nameCont.appendChild(size);
-
   let detail = document.createElement("span");
-  detail.innerText = drink.style;
+  detail.innerText = drink.description;
   detail.classList.add("item-detail");
 
-  let abv = document.createElement("span");
-  abv.innerText = drink.abv;
-  abv.classList.add("item-detail");
+  let li = document.createElement("li");
+  li.classList.add("list-item");
+  li.appendChild(name);
+  li.appendChild(detail);
 
-  let detailCont = document.createElement("li");
-  detailCont.classList.add("list-detail");
-  detailCont.appendChild(detail);
-  detailCont.appendChild(abv);
+  //OLD ITEMS WITH ABV AND DRINK STYLE
+  // let size = document.createElement("span");
+  // size.classList.add("item-size");
+  // size.innerText = drink.size;
 
-  let listCont = document.createElement("li");
-  listCont.classList.add("list-item");
-  listCont.appendChild(nameCont);
-  listCont.appendChild(detailCont);
+  // let nameCont = document.createElement("li");
+  // nameCont.classList.add("list-name");
+  // nameCont.appendChild(name);
+  // nameCont.appendChild(size);
 
-  const list = document.getElementById(`${drink.type}-list`);
-  list.appendChild(listCont);
+  // let abv = document.createElement("span");
+  // abv.innerText = drink.abv;
+  // abv.classList.add("item-detail");
+
+  // let detailCont = document.createElement("li");
+  // detailCont.classList.add("list-detail");
+  // detailCont.appendChild(detail);
+  // detailCont.appendChild(abv);
+
+  // let listCont = document.createElement("li");
+  // listCont.classList.add("list-item");
+  // listCont.appendChild(nameCont);
+  // listCont.appendChild(detailCont);
+
+  const list = document.getElementById(`${menu}-list`);
+  list.appendChild(li);
 }
 
-setupPage();
+menus.forEach(setupPage);
